37f95383d96e2110bc3d8e3823c5e2724d038f38
Andrei Moldovan <andreimoldovan6@gmail.com>
2022-11-05 15:17:14 +0200

    Rezolvare Lab4

--- Models/Author.cs
+++ Models/Author.cs
@@ -1,9 +1,20 @@
-﻿namespace Moldovan_Andrei.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace Moldovan_Andrei.Models
 {
     public class Author
     {
         public int ID { get; set; }
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
     }
 }
---
+++ Models/ViewModels/PublisherIndexData.cs
@@ -0,0 +1,11 @@
+﻿using Moldovan_Andrei.Models;
+namespace Moldovan_Andrei.ViewModels
+{
+    public class PublisherIndexData
+    {
+
+        public IEnumerable<Publisher> Publishers { get; set; }
+        public IEnumerable<Book> Books { get; set; }
+
+    }
+}
